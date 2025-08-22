0bcd03c47b84742fd094eaa03df5c031d5e49430
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2025-08-22 16:22:58 +0100

    Polished data pipeline, improved get_parquet script

--- scripts/get_parquet.py
+++ scripts/get_parquet.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import os
 
+
 # Fetch page
 url = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
 resp = requests.get(url)
@@ -19,18 +20,17 @@ pattern = re.compile(
     r"yellow[_a-z]*?20\d{2}-\d{2}\.parquet"
 )
 yellow_links = [link for link in links if pattern.search(link)]
-print(yellow_links)
-# # Download files and save into data dir
-# SAVE_DIR = "../data/raw"
-# os.makedirs(SAVE_DIR, exist_ok=True) 
 
-# for link in yellow_links:
-#     filename = link.split("/")[-1]
-#     dest = os.path.join(SAVE_DIR, filename)
-#     print(f"Downloading {filename}...")
+# Save the links to a txt, then use bash to download
+with open("parquet_files.txt", "w") as f:
+    for link in yellow_links:
+        f.write(link + "\n")
 
-#     r = requests.get(link)
-#     with open(dest, "wb") as f:
-#         f.write(r.content)
+# In bash:
+
+# If it doesn't exist already make the data/raw directory
+# mkdir -p ../data/raw
+
+# wget -i urls.txt -P ../data/raw
 
 
