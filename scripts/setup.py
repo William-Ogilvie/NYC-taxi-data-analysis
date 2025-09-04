334bca085fe3007c31f765b79af0a701e9f9f4a6
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2025-09-04 12:13:58 +0100

    Cleaned training_helpers.py

--- scripts/setup.py
+++ scripts/setup.py
@@ -14,11 +14,11 @@ with open(CONFIG_PATH, "r") as f:
 # We are now going to make all the directories we need for the project if they don't already exist
 
 # Data directories
-raw_data_dir = PROJECT_ROOT / Path(config["data"]["raw_path"])
-processed_data_dir = PROJECT_ROOT / Path(config["data"]["processed_path"])
-saved_objects_dir = PROJECT_ROOT / Path(config["data"]["saved_objects_path"])
-figures_dir = PROJECT_ROOT / Path(config["data"]["figures_path"])
-maps_dir = PROJECT_ROOT / Path(config["data"]["maps_path"])
+raw_data_dir = PROJECT_ROOT / Path(config["data"]["data_path"]) / Path(config["data"]["raw_path"])
+processed_data_dir = PROJECT_ROOT / Path(config["data"]["data_path"]) / Path(config["data"]["processed_path"])
+saved_objects_dir = PROJECT_ROOT / Path(config["data"]["data_path"]) / Path(config["data"]["saved_objects_path"])
+figures_dir = PROJECT_ROOT / Path(config["data"]["reports_path"]) / Path(config["data"]["figures_path"])
+maps_dir = PROJECT_ROOT / Path(config["data"]["reports_path"]) / Path(config["data"]["maps_path"])
 
 # Create the directories if they don't exist already exist
 raw_data_dir.mkdir(parents= True, exist_ok= True)
--- src/jfk_taxis/training_helpers.py
+++ src/jfk_taxis/training_helpers.py
@@ -13,10 +13,21 @@ with open(CONFIG_PATH, "r") as f:
     config = yaml.safe_load(f)
 
 # Saved objects path
-SAVED_OBJECTS_PATH = PROJECT_ROOT / Path(config["data"]["saved_objects_path"])
+SAVED_OBJECTS_PATH = PROJECT_ROOT / Path(config["data"]["data_path"]) / Path(config["data"]["saved_objects_path"])
 
 # Create a string of the path for use when saving
 SAVE_DIR = str(SAVED_OBJECTS_PATH.resolve())
+
+# Name saving conventions
+MODEL = config["saving"]["model_file_suffix"]
+HYBRID = config["saving"]["hybrid_file_suffix"]
+DP = config["saving"]["dp_file_suffix"]
+DESIGN = config["saving"]["design_file_suffix"]
+TARGET = config["saving"]["target_file_suffix"]
+LINEAR_KEYS = config["saving"]["linear_keys_file_preffix"]
+NON_LINEAR_KEYS = config["saving"]["non_linear_keys_file_preffix"]
+LAGS_PREFFIX = config["saving"]["lags_preffix"]
+HYPERPARAMS_PREFFIX = config["saving"]["hyperparams_preffix"]
 
 # Function saves the design and target matrices, the models themselves and their deterministic processes into .pkl objects using joblib
 def save_models(linear_models: dict, non_linear_models: dict, sig: str):
@@ -31,20 +42,20 @@ def save_models(linear_models: dict, non_linear_models: dict, sig: str):
 
     # Save models
     for key, value in linear_models.items():
-        joblib.dump(value[0], f"{SAVE_DIR}/{key}_{sig}_model.pkl")
-        joblib.dump(value[1], f"{SAVE_DIR}/{key}_{sig}_dp.pkl")
-        joblib.dump(value[2], f"{SAVE_DIR}/{key}_{sig}_hybrid.pkl")
+        joblib.dump(value[0], SAVED_OBJECTS_PATH / f"{key}_{sig}_{MODEL}")  
+        joblib.dump(value[1], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
+        joblib.dump(value[2], SAVED_OBJECTS_PATH / f"{key}_{sig}_{HYBRID}")
 
 
     for key, value in non_linear_models.items():
-        joblib.dump(value[0], f"{SAVE_DIR}/{key}_{sig}_model.pkl")
-        joblib.dump(value[1], f"{SAVE_DIR}/{key}_{sig}_dp.pkl")
-        joblib.dump(value[2], f"{SAVE_DIR}/{key}_{sig}_hybrid.pkl")
+        joblib.dump(value[0], SAVED_OBJECTS_PATH / f"{key}_{sig}_{MODEL}")
+        joblib.dump(value[1], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
+        joblib.dump(value[2], SAVED_OBJECTS_PATH / f"{key}_{sig}_{HYBRID}")
 
 
     # Save the keys so we can reload easily
-    joblib.dump(list(linear_models.keys()), f"{SAVE_DIR}/Linear_keys_{sig}_models.pkl") 
-    joblib.dump(list(non_linear_models.keys()), f"{SAVE_DIR}/Non_linear_keys_{sig}_models.pkl") 
+    joblib.dump(list(linear_models.keys()), SAVED_OBJECTS_PATH / f"{LINEAR_KEYS}_{sig}_{MODEL}") 
+    joblib.dump(list(non_linear_models.keys()), SAVED_OBJECTS_PATH / f"{NON_LINEAR_KEYS}_{sig}_{MODEL}") 
 
 # Function saves the design, target and deterministic process used to train the models
 def save_design(linear_design: dict, non_linear_design: dict, sig: str):
@@ -56,25 +67,25 @@ def save_design(linear_design: dict, non_linear_design: dict, sig: str):
     
     # Save design, target and deterministic process matricies
     for key, value in linear_design.items():
-        joblib.dump(value[0], f"{SAVE_DIR}/{key}_{sig}_design.pkl")
-        joblib.dump(value[1], f"{SAVE_DIR}/{key}_{sig}_target.pkl")
-        joblib.dump(value[2], f"{SAVE_DIR}/{key}_{sig}_dp1.pkl")
+        joblib.dump(value[0], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DESIGN}")
+        joblib.dump(value[1], SAVED_OBJECTS_PATH / f"{key}_{sig}_{TARGET}")
+        joblib.dump(value[2], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
 
     for key, value in non_linear_design.items():
-        joblib.dump(value[0], f"{SAVE_DIR}/{key}_{sig}_design.pkl")
-        joblib.dump(value[1], f"{SAVE_DIR}/{key}_{sig}_target.pkl")
-        joblib.dump(value[2], f"{SAVE_DIR}/{key}_{sig}_dp1.pkl")
+        joblib.dump(value[0], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DESIGN}")
+        joblib.dump(value[1], SAVED_OBJECTS_PATH / f"{key}_{sig}_{TARGET}")
+        joblib.dump(value[2], SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
 
     # Save the keys so we can reload easily
-    joblib.dump(list(linear_design.keys()), f"{SAVE_DIR}/Linear_keys_{sig}_design.pkl") # same keys for linear design
-    joblib.dump(list(non_linear_design.keys()), f"{SAVE_DIR}/Non_linear_keys_{sig}_design.pkl") # same keys for non linear design
+    joblib.dump(list(linear_design.keys()), SAVED_OBJECTS_PATH / f"{LINEAR_KEYS}_{sig}_{DESIGN}") # same keys for linear design
+    joblib.dump(list(non_linear_design.keys()), SAVED_OBJECTS_PATH / f"{NON_LINEAR_KEYS}_{sig}_{DESIGN}") # same keys for non linear design
 
 
 # Loads the linear and non linear models stored under the signature sig
 def load_models(sig: str):
     # Load keys
-    linear_keys = joblib.load(f"{SAVE_DIR}/Linear_keys_{sig}_models.pkl")
-    non_linear_keys = joblib.load(f"{SAVE_DIR}/Non_linear_keys_{sig}_models.pkl")
+    linear_keys = joblib.load(SAVED_OBJECTS_PATH / f"{LINEAR_KEYS}_{sig}_{MODEL}")
+    non_linear_keys = joblib.load(SAVED_OBJECTS_PATH / f"{NON_LINEAR_KEYS}_{sig}_{MODEL}")
 
     # Store models into dicts to return
     non_linear_models_loaded = {}
@@ -82,15 +93,15 @@ def load_models(sig: str):
 
     # Loop through both sets of keys and load all components
     for key in linear_keys:
-        model = joblib.load(f"{SAVE_DIR}/{key}_{sig}_model.pkl")
-        dp = joblib.load(f"{SAVE_DIR}/{key}_{sig}_dp.pkl")
-        hybrid = joblib.load(f"{SAVE_DIR}/{key}_{sig}_hybrid.pkl")
+        model = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{MODEL}")
+        dp = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
+        hybrid = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{HYBRID}")
         linear_models_loaded[key] = (model, dp, hybrid)
 
     for key in non_linear_keys:
-        model = joblib.load(f"{SAVE_DIR}/{key}_{sig}_model.pkl")
-        dp = joblib.load(f"{SAVE_DIR}/{key}_{sig}_dp.pkl")
-        hybrid = joblib.load(f"{SAVE_DIR}/{key}_{sig}_hybrid.pkl")
+        model = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{MODEL}")
+        dp = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
+        hybrid = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{HYBRID}")
         non_linear_models_loaded[key] = (model, dp, hybrid)
 
 
@@ -99,8 +110,8 @@ def load_models(sig: str):
 def load_design(sig: str):
 
     # Load keys
-    linear_keys = joblib.load(f"{SAVE_DIR}/Linear_keys_{sig}_design.pkl")
-    non_linear_keys = joblib.load(f"{SAVE_DIR}/Non_linear_keys_{sig}_design.pkl")
+    linear_keys = joblib.load(SAVED_OBJECTS_PATH / f"{LINEAR_KEYS}_{sig}_{DESIGN}")
+    non_linear_keys = joblib.load(SAVED_OBJECTS_PATH / f"{NON_LINEAR_KEYS}_{sig}_{DESIGN}")
 
     # Create dicts to store design, target and deterministic process to load
     non_linear_design_loaded = {}
@@ -108,40 +119,40 @@ def load_design(sig: str):
 
     # Loop through both sets of keys and load all components
     for key in linear_keys:
-        X = joblib.load(f"{SAVE_DIR}/{key}_{sig}_design.pkl")
-        y = joblib.load(f"{SAVE_DIR}/{key}_{sig}_target.pkl")
-        dp = joblib.load(f"{SAVE_DIR}/{key}_{sig}_dp1.pkl")
+        X = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DESIGN}")
+        y = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{TARGET}")
+        dp = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
         linear_design_loaded[key] = (X, y, dp)
 
     for key in non_linear_keys:
-        X = joblib.load(f"{SAVE_DIR}/{key}_{sig}_design.pkl")
-        y = joblib.load(f"{SAVE_DIR}/{key}_{sig}_target.pkl")
-        dp = joblib.load(f"{SAVE_DIR}/{key}_{sig}_dp1.pkl")
+        X = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DESIGN}")
+        y = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{TARGET}")
+        dp = joblib.load(SAVED_OBJECTS_PATH / f"{key}_{sig}_{DP}")
         non_linear_design_loaded[key] = (X, y, dp)
 
     return linear_design_loaded, non_linear_design_loaded
 
 # Function to save lags, exists primarily to ensure that everything ends up in the same folder at run time
 def save_lags(lags: list, series_type: str, sig: str):
-    joblib.dump(lags, f"{SAVE_DIR}/sig_lags_{series_type}_{sig}.pkl")
+    joblib.dump(lags, SAVED_OBJECTS_PATH / f"{LAGS_PREFFIX}_{series_type}_{sig}.pkl")
 
 # Function to load lags
 def load_lags(series_type: str, sig: str):
-    return joblib.load(f"{SAVE_DIR}/sig_lags_{series_type}_{sig}.pkl")
+    return joblib.load(SAVED_OBJECTS_PATH / f"{LAGS_PREFFIX}_{series_type}_{sig}.pkl")
 
 
 # Function to save hyperparams
 def save_hyperparams(hyperparams: dict, sig: str):
-    joblib.dump(hyperparams, f"{SAVE_DIR}/hyperparams_{sig}.pkl")
+    joblib.dump(hyperparams, SAVED_OBJECTS_PATH / f"{HYPERPARAMS_PREFFIX}_{sig}.pkl")
 
 # Function to load hyperparams
 def load_hyperparams(sig: str):
-    return joblib.load(f"{SAVE_DIR}/hyperparams_{sig}.pkl")
+    return joblib.load(SAVED_OBJECTS_PATH / f"{HYPERPARAMS_PREFFIX}_{sig}.pkl")
 
 # Function to save any object
 def save_obj(obj, sig: str):
-    joblib.dump(obj, f"{SAVE_DIR}/{sig}.pkl")
+    joblib.dump(obj, SAVED_OBJECTS_PATH / f"{sig}.pkl")
 
 # Function to load any object
 def load_obj(sig: str):
-    return joblib.load(f"{SAVE_DIR}/{sig}.pkl") 
+    return joblib.load(SAVED_OBJECTS_PATH / f"{sig}.pkl")
