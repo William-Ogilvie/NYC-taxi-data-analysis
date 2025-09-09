2401a3044f9ee5f5298ea16d6b69fceab695c8dd
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2025-09-09 20:37:44 +0100

    more shap

--- src/jfk_taxis/__init__.py
+++ src/jfk_taxis/__init__.py
@@ -6,7 +6,6 @@ from .training_helpers import save_models, save_design, load_models, load_design
 from .hyperparam_helpers import create_val_data, wrapped_objective, split_params, test_hyperparams
 from .modelling_helpers import create_train_save_models
 from .loading_helpers import load_config
-from .shap_helpers import load_design_for_shap, fit_non_linear_model, fit_linear_model
 
 # What can be imported from src
 __all__ = ["run_forecasts", "process_taxi_data", "taxi_data_visuals",
@@ -18,8 +17,8 @@ __all__ = ["run_forecasts", "process_taxi_data", "taxi_data_visuals",
             "load_models", "load_design", "save_lags", "load_lags", "forecast",
             "create_val_data", "wrapped_objective",  "save_hyperparams", "load_hyperparams",
             "save_obj", "load_obj", "create_train_save_models", "split_params", "test_hyperparams", "load_config",
-            "create_save_listed_adjusted_choropleths", "multiplot_choropleths", "load_design_for_shap",
-            "fit_non_linear_model", "fit_linear_model", "load_ts_data", "split_test_train_sets", "load_process_lags"]
+            "create_save_listed_adjusted_choropleths", "multiplot_choropleths", 
+            "load_ts_data", "split_test_train_sets", "load_process_lags"]
 
 
 
