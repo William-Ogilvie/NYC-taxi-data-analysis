a6f11ad5e0ad3f86615f8cfc3aae3931ed8003be
William-Ogilvie <william.ogilvie@st-hughs.ox.ac.uk>
2025-09-30 15:28:25 +0100

    added more training helpers tests

--- tests/test_training_helpers.py
+++ tests/test_training_helpers.py
@@ -95,3 +95,99 @@ def test_split_test_train_sets():
     # Sanity check that the boundaries are correct themselves
     assert pd.to_datetime(config["modelling"]["ts_daily_train_boundary"]) == pd.to_datetime(config["modelling"]["ts_daily_test_boundary"]) - pd.Timedelta(days=1), "ts_daily_train_boundary is not one day before ts_daily_test_boundary"
     assert pd.to_datetime(config["modelling"]["ts_hourly_train_boundary"]) == pd.to_datetime(config["modelling"]["ts_hourly_test_boundary"]) - pd.Timedelta(hours=1), "ts_hourly_train_boundary is not one hour before ts_hourly_test_boundary"
+
+def test_load_process_lags():
+    """ test load_process_lags function in training_helpers.py
+    """    
+    from jfk_taxis import training_helpers
+    from jfk_taxis import load_config
+
+    # Load config and the lags we expect
+    config, PROJECT_ROOT =  load_config()
+
+    expected_daily_lags = config["modelling"]["daily_lags"]
+    expected_hourly_lags = config["modelling"]["hourly_lags"]
+    expected_used_hourly_lags = expected_hourly_lags[:config["modelling"]["hourly_num_lags"]]
+    expected_extra_hourly_lags = config["modelling"]["hourly_extra_lags"]
+
+    daily_lags, used_hourly_lags = training_helpers.load_process_lags()
+
+    # Check that the lags are correct
+    assert daily_lags == expected_daily_lags, f"daily_lags {daily_lags} does not match expected {expected_daily_lags}"
+    assert len(used_hourly_lags) == len(expected_used_hourly_lags) + len(expected_extra_hourly_lags), f"used_hourly_lags {used_hourly_lags} does not have the correct length"
+    for lag in used_hourly_lags:
+        assert lag in expected_used_hourly_lags + expected_extra_hourly_lags, f"lag {lag} in used_hourly_lags {used_hourly_lags} is not in expected {expected_used_hourly_lags + expected_extra_hourly_lags}"
+
+def test_save_models():
+    """ test save_models function in training_helpers.py
+    """    
+    from jfk_taxis import training_helpers
+    from jfk_taxis import load_config
+    from sklearn.linear_model import LinearRegression
+    from xgboost import XGBRegressor
+    from statsmodels.tsa.deterministic import DeterministicProcess
+    from test_modelling_helpers import create_ts
+
+    # Load config
+    config, PROJECT_ROOT =  load_config()
+
+    # We will use a daily time series for the first model and an hourly one for the second
+    ts_daily = create_ts("D")
+    ts_hourly = create_ts("h")
+
+    # To test that the save models function works we will create some dummy models, and save them and check they exist in the correct form in the right directory
+    # we won't actually load them back in this test as that will be done in the test_load_models function
+
+    # We create two dummy linear models (one hybrid and one not) and two dummy non-linear models 
+    dummy_linear_model = LinearRegression()  
+    dummy_hybrid_component = XGBRegressor()
+    dummy_dp_linear_daily = DeterministicProcess(index = ts_daily.index, order=3, seasonal=True, constant = True)
+    dummy_dp_linear_hourly = DeterministicProcess(index = ts_hourly.index, order=3, seasonal=True, constant = True) 
+    dummy_lags_linear = [1,2,3,4,5]
+    dummy_lags_linear_2 = [1,2,3]
+    
+    dummmy_non_linear_model = XGBRegressor()
+    dummy_non_linear_hybrid = None
+    dummy_dp_non_linear_daily = DeterministicProcess(index = ts_daily.index, constant = False, order = 0, seasonal = True)
+    dummy_dp_non_linear_hourly = DeterministicProcess(index = ts_hourly.index, constant = False, order = 0, seasonal = True) 
+    dummy_lags_non_linear = [1,2,3,4,5,6,7,8,9,10]
+    dummy_lags_non_linear_2 = [1,2,3,4,5]
+
+    linear_model_names = ["test_linear_model", "test_linear_model_2"]
+    non_linear_model_names = ["test_non_linear_model", "test_non_linear_model_2"]
+
+
+    linear_models = {
+        linear_model_names[0]: (dummy_linear_model, dummy_dp_linear_daily, dummy_hybrid_component, dummy_lags_linear), 
+        linear_model_names[1]: (dummy_linear_model, dummy_dp_linear_hourly, None, dummy_lags_linear_2) 
+    }
+
+    non_linear_models = {
+        non_linear_model_names[0]: (dummmy_non_linear_model, dummy_dp_non_linear_daily, dummy_non_linear_hybrid, dummy_lags_non_linear),
+        non_linear_model_names[1]: (dummmy_non_linear_model, dummy_dp_non_linear_hourly, dummy_non_linear_hybrid, dummy_lags_non_linear_2)
+    }
+
+    # Save the models
+    sig = "test_model_sig"
+    training_helpers.save_models(linear_models, non_linear_models, sig)
+
+    # Now check that the models exist in the correct directory
+    SAVED_OBJECTS_DIR = PROJECT_ROOT / config["data"]["data_path"] / config["data"]["saved_objects_path"]
+
+    # Saving constants
+    MODEL = config["saving"]["model_file_suffix"]
+    HYBRID = config["saving"]["hybrid_file_suffix"]
+    DP = config["saving"]["dp_file_suffix"]
+    LAGS =  config["saving"]["lags_file_suffix"]
+
+    expected_files = [
+        SAVED_OBJECTS_DIR / f"{name}_{sig}_{suffix}"
+        for name in linear_model_names + non_linear_model_names
+        for suffix in [MODEL, HYBRID, DP, LAGS]
+    ]
+
+    # Check that the files exist
+    for f in expected_files:
+        assert f.exists(), f"Expected file {f} does not exist"
+
+
